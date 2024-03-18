f1701da18a3c17bfc9a81a1980db889ea0caa061
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2024-03-18 09:56:35 +0530

    Update HeadingBefore.jsx

--- frontend/src/components/HeadingBefore.jsx
+++ frontend/src/components/HeadingBefore.jsx
@@ -3,7 +3,7 @@ import './Reports.css'
 const HeadingBefore = () => {
   return (
     <div >
-        <strong className='personalized-report-head'>All the data you need to Build a successful Business </strong><span className='before-head' >is just a click away</span>
+        <strong className='personalized-report-head'>All the data you need to Build a successful Business. </strong><span className='before-head' >is just a click away</span>
         <br />
         <br />
            
