7f73b88a463ddfb398773554d35a8b0c6aa3acef
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2025-01-27 16:31:53 +0530

    Update ReportsDisplay.js

--- frontend/src/components/ReportsDisplay.js
+++ frontend/src/components/ReportsDisplay.js
@@ -40,7 +40,7 @@ const ReportsDisplay = () => {
 
   // Fetch pre-signed URL from the API Gateway
   useEffect(() => {
-    const fetchPresignedUrl = async () => {
+    const fetchPresignedUrl = {
       try {
         const response = await fetch('https://vtwyu7hv50.execute-api.ap-south-1.amazonaws.com/default/RBR_report_pre-signed_URL', {
           method: 'POST',
