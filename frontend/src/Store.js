94453efe44f167d4ab13683faf8a5c3f94d84d79
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2025-04-07 14:58:25 +0530

    Update Store.js

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -3,7 +3,7 @@ import { createContext, useReducer } from "react";
 export const Store = createContext();
 
 const initialState = {
-  isLogin: localStorage.getItem("isLogin") === "true" || false, // Add persistence
+  isLogin: localStorage.getItem("isLogin") === "true" || false,
   name: '',
   phone: '',
   email: '',
@@ -16,7 +16,7 @@ const reducer = (state, action) => {
     case 'SET_PRICE':
       return { ...state, totalPrice: action.payload };
     case 'USER_LOGIN':
-      localStorage.setItem("isLogin", action.payload); // Persist isLogin
+      localStorage.setItem("isLogin", action.payload);
       return { ...state, isLogin: action.payload };
     case 'SET_NAME':
       return { ...state, name: action.payload };
@@ -26,6 +26,9 @@ const reducer = (state, action) => {
       return { ...state, email: action.payload };
     case 'SET_REPORT_STATUS':
       return { ...state, status: !state.status };
+    case 'LOGOUT': // Added
+      localStorage.setItem("isLogin", "false");
+      return { ...state, isLogin: false, name: '', phone: '', email: '' };
     default:
       return state;
   }
