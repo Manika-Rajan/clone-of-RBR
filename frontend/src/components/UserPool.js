f9915a2d655b30def04594f55f32df4f0d869a37
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2025-07-08 12:11:30 +0530

    Delete frontend/src/components/UserPool.js

--- frontend/src/components/UserPool.js
+++
@@ -1,7 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-const poolData = {
-    UserPoolId : "ap-south-1_9meTm9obo",
-    ClientId: "1glle7bvkfkrqt77p19jpqelss"
-}
-const userPool = new CognitoUserPool(poolData);
-export default userPool;
