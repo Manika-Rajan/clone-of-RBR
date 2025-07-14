20519fac2bbeef01e2354af0bd0a4606dc22773a
Manika Rajan <manikya93@gmail.com>
2025-07-14 16:06:38 +0530

    Fix bootstrap import and build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './Store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //configure(awsmobile);
 
