04aeba7edf54c1f0856a6cc52502354e6cd8b694
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2025-07-29 15:24:42 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -105,7 +105,7 @@ const Navbar = (props) => {
                     <button className="nav-link login-btn" onClick={() => {
                       resetModal();
                       setOpenModel(true);
-                    }}style={{ backgroundColor: '#0263c7', color: 'white', opacity: 1, visibility: 'visible', zIndex: 1000 }}
+                    }}
                       >LOGIN</button>
                   </li>
                 )}
