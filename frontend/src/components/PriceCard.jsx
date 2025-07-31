31bdaa48f05bf56e33f254ae32fc77f0769244f7
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2025-07-31 09:53:36 +0530

    Update PriceCard.jsx

--- frontend/src/components/PriceCard.jsx
+++ frontend/src/components/PriceCard.jsx
@@ -5,7 +5,7 @@ const PriceCard = ({price}) => {
     <div className='price-card' style={{margin:"auto"}}>
         <div className="card-text">
         <h5>Total Price</h5>
-      <p>*excluding gst</p>
+      <p>*including GST</p>
         </div>
        <div className="price">
        <strong>₹{price}</strong>
