7d966b9bd5d16592f94ae549441e93d25dd50f57
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2025-08-22 16:52:26 +0530

    Update Store.js

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -2,36 +2,44 @@ import React, { createContext, useReducer, useContext } from 'react';
 
 const initialState = {
   isLogin: false,
-  userId: '',
-  name: '',
-  email: '',
-  phone: '',
-  loginState: { number: '', otpInput: '', name: '', email: '', responseMessage: '', error: '', otpSent: false, isLoading: false, updateTrigger: 0 },
-  loginPhase: 0,
+  userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
+  loginState: {
+    number: '',
+    otpInput: '',
+    otpSent: false,
+    updateTrigger: 0,
+    isLoading: false,
+    error: '',
+    responseMessage: '',
+  },
 };
 
-export const Store = createContext(initialState);
+export const Store = createContext({
+  state: initialState,
+  dispatch: () => null,
+});
 
 const reducer = (state, action) => {
   switch (action.type) {
     case 'USER_LOGIN':
-      return { ...state, ...action.payload, loginPhase: 2 };
-    case 'SET_PHONE':
-      return { ...state, phone: action.payload };
+      return { ...state, isLogin: true, userInfo: action.payload };
+    case 'USER_LOGOUT':
+      return { ...state, isLogin: false, userInfo: null, loginState: initialState.loginState };
     case 'UPDATE_LOGIN_FIELD':
       return { ...state, loginState: { ...state.loginState, [action.field]: action.value } };
     case 'SET_LOGIN_STATE':
       return { ...state, loginState: { ...state.loginState, ...action.payload } };
+    case 'SET_PHONE':
+      return { ...state, loginState: { ...state.loginState, number: action.payload.replace('+91', '') } };
     default:
       return state;
   }
 };
 
-export const StoreProvider = (props) => {
+export const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const value = { state, dispatch };
-  console.log('StoreProvider rendering with state:', state);
-  return <Store.Provider value={value}>{props.children}</Store.Provider>;
+  console.log('Store.js:33 StoreProvider rendering with state:', state);
+  return <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>;
 };
 
 export const useStore = () => useContext(Store);
