af050c474c31227b2622e4f8e40bb356af09d631
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2025-08-23 09:17:31 +0530

    Update Store.js

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer, useContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 const initialState = {
   isLogin: false,
@@ -12,6 +12,28 @@ const initialState = {
     error: '',
     responseMessage: '',
   },
+  status: false,
+  name: '',
+  email: '',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'USER_LOGIN':
+      return { ...state, isLogin: true, userInfo: action.payload, status: false };
+    case 'USER_LOGOUT':
+      return { ...state, isLogin: false, userInfo: null, loginState: initialState.loginState, status: false };
+    case 'UPDATE_LOGIN_FIELD':
+      return { ...state, loginState: { ...state.loginState, [action.field]: action.value } };
+    case 'SET_LOGIN_STATE':
+      return { ...state, loginState: { ...state.loginState, ...action.payload } };
+    case 'SET_PHONE':
+      return { ...state, loginState: { ...state.loginState, number: action.payload.replace('+91', '') } };
+    case 'SET_REPORT_STATUS':
+      return { ...state, status: true, name: state.userInfo?.name || '', email: state.userInfo?.email || '' };
+    default:
+      return state;
+  }
 };
 
 export const Store = createContext({
@@ -19,28 +41,8 @@ export const Store = createContext({
   dispatch: () => null,
 });
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'USER_LOGIN':
-      return { ...state, isLogin: true, userInfo: action.payload };
-    case 'USER_LOGOUT':
-      return { ...state, isLogin: false, userInfo: null, loginState: initialState.loginState };
-    case 'UPDATE_LOGIN_FIELD':
-      return { ...state, loginState: { ...state.loginState, [action.field]: action.value } };
-    case 'SET_LOGIN_STATE':
-      return { ...state, loginState: { ...state.loginState, ...action.payload } };
-    case 'SET_PHONE':
-      return { ...state, loginState: { ...state.loginState, number: action.payload.replace('+91', '') } };
-    default:
-      return state;
-  }
-};
-
 export const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log('Store.js:33 StoreProvider rendering with state:', state);
   return <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>;
 };
-
-export const useStore = () => useContext(Store);
-export default StoreProvider;
