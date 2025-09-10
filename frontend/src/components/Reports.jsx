90b68c1a2e618497e1217f3b7e3edbe279c361ea
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2025-09-10 16:09:38 +0530

    Update Reports.jsx

--- frontend/src/components/Reports.jsx
+++ frontend/src/components/Reports.jsx
@@ -11,7 +11,7 @@ import vector from '../assets/vector.svg';
 import black from '../assets/black.svg';
 import { useNavigate } from 'react-router-dom';
 import { Store } from '../Store';
-import worldMapBg from '../assets/world-map-bg.png';
+import worldMapBg from "../assets/world-map-bg.png";  // ✅ correct import
 
 const Reports = () => {
   const navigate = useNavigate();
