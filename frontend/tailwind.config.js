5a84e4e649649fbff180a34980700e3bc78dea0e
Manika Rajan <manikya93@gmail.com>
2025-10-02 12:32:23 +0530

    Pin Tailwind to v3 for CRA + fix Amplify build

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  // If Bootstrap is also loaded and resets clash, uncomment next line:
+  // corePlugins: { preflight: false },
   theme: { extend: {} },
   plugins: [],
 };
