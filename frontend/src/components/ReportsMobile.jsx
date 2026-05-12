7c77f159476663c33a87c22e1ceeb035f7b550c7
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2026-05-12 10:17:41 +0530

    Update ReportsMobile.jsx

--- frontend/src/components/ReportsMobile.jsx
+++ frontend/src/components/ReportsMobile.jsx
@@ -1927,9 +1927,9 @@ const runSampleSearch = (query) => {
 };
 
   return (
-    <div className="min-h-screen bg-white flex flex-col items-center px-4 pt-3 pb-10 relative">
+    <div className="min-h-screen bg-white flex flex-col items-center px-4 pt-24 pb-10 relative">
 {/* Sample Reports */}
-<div className="w-full flex justify-end mt-2 mb-5 px-1">
+<div className="absolute top-3 right-0 sm:right-1">
   <button
     type="button"
     onClick={() => setSamplesOpen(true)}
